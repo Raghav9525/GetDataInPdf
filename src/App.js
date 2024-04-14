aec2b7816e8287a9f9c322630464005bcf8e6355
Raghav9525 <kshubham9525.gmail.com>
2024-04-14 17:26:45 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./components/Form";
+import Form1 from "./components/Form1";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from 'react-router-dom'
+import Dashboard from "./components/Dashboard";
+import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Form /> */}
+      <BrowserRouter>
+        <Navbar />
+        <Dashboard />
+        <Footer />
+      </BrowserRouter>
+
     </div>
   );
 }
